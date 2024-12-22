84724d0dbb6cadc54b60acbc3408e0ad3b0b7215
RAVI KUMAR <ss7524294@gmail.com>
2024-12-22 18:40:48 +0530

    22

--- d1mm4/lib/main.dart
+++ d1mm4/lib/main.dart
@@ -21,77 +21,345 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    final screenWidth = MediaQuery.of(context).size.width;
+  _HomePageState createState() => _HomePageState();
+}
 
+class _HomePageState extends State<HomePage> {
+  int _currentStep = 0;
+  final List<int> stepProgress = [20, 40, 60, 80, 100];
+  final List<String> stepTexts = [
+    "Complete your profile",
+    "Verify yourself",
+    "Complete verification",
+    "Create your plan",
+    "Create website"
+  ];
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('D1MM4'),
       ),
-      body: Container(
-        margin: EdgeInsets.only(
-          top: 80,
-          left: screenWidth * 0.025,
-          right: screenWidth * 0.025,
-        ),
-        child: Card(
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(16),
-          ),
-          color: Color.fromRGBO(85, 166, 196, 0.3),
-          child: SizedBox(
-            width: double.infinity,
-            height: 250,
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const Text(
-                    'Welcome Back!',
-                    style: TextStyle(
-                      fontSize: 24, 
-                      fontWeight: FontWeight.bold, 
-                      color: Colors.white,
-                      fontFamily: 'Roboto', 
-                      fontStyle: FontStyle.italic, 
+      body: SingleChildScrollView(
+        child: Center(
+          child: Container(
+            margin: const EdgeInsets.only(
+                top: 80, left: 16, right: 16), // Adjusted margin
+            child: Column(
+              children: [
+                Card(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(16),
+                  ),
+                  color: const Color.fromRGBO(85, 166, 196, 0.3),
+                  child: SizedBox(
+                    width: 344,
+                    height: 199,
+                    child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          // Combined Title and Message in the Same Container
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: const [
+                              Text(
+                                'Welcome Back!',
+                                style: TextStyle(
+                                  fontSize: 24,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.white,
+                                  fontFamily: 'Roboto',
+                                  fontStyle: FontStyle.italic,
+                                ),
+                              ),
+                              SizedBox(height: 3), // Further reduced margin
+                              Text(
+                                'You are One-Step Closer to creating your App',
+                                style: TextStyle(
+                                    fontSize: 14, color: Colors.white),
+                              ),
+                            ],
+                          ),
+
+                          // Stepper and Progress Container
+                          Column(
+                            children: [
+                              // Stepper and Progress Row
+                              Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Row(
+                                    children: List.generate(5, (index) {
+                                      return Row(
+                                        children: [
+                                          GestureDetector(
+                                            onTap: () {
+                                              setState(() {
+                                                _currentStep = index;
+                                              });
+                                            },
+                                            child: Column(
+                                              children: [
+                                                CircleAvatar(
+                                                  backgroundColor:
+                                                      _currentStep >= index
+                                                          ? Color.fromRGBO(
+                                                              184, 254, 34, 1)
+                                                          : Colors.grey,
+                                                  child: Text(
+                                                    (index + 1).toString(),
+                                                    style: const TextStyle(
+                                                        color: Colors.white),
+                                                  ),
+                                                ),
+                                              ],
+                                            ),
+                                          ),
+                                          if (index < 4)
+                                            Container(
+                                              alignment: Alignment.center,
+                                              child: Container(
+                                                width: 8, // Reduced width
+                                                height: 6, // Thicker line
+                                                color: _currentStep > index
+                                                    ? Color.fromRGBO(
+                                                        184, 254, 34, 1)
+                                                    : Colors.grey,
+                                              ),
+                                            ),
+                                        ],
+                                      );
+                                    }),
+                                  ),
+                                  const Text(
+                                    '|',
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 24,
+                                    ),
+                                  ),
+                                  Text(
+                                    '${stepProgress[_currentStep]}%',
+                                    style: const TextStyle(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 24,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+
+                          // Button
+                          ElevatedButton(
+                            onPressed: () {
+                              setState(() {
+                                if (_currentStep < 4) {
+                                  _currentStep++;
+                                }
+                              });
+                            },
+                            child: Text(
+                              stepTexts[_currentStep],
+                              style: const TextStyle(fontSize: 18),
+                            ),
+                            style: ButtonStyle(
+                              backgroundColor: MaterialStateProperty.all(
+                                const Color.fromRGBO(184, 254, 34, 1),
+                              ),
+                              minimumSize: MaterialStateProperty.all(
+                                  const Size(320, 30)), // Reduced button height
+                              padding: MaterialStateProperty.all(
+                                  const EdgeInsets.symmetric(vertical: 8)),
+                              shape: MaterialStateProperty.all(
+                                RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(8),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
                   ),
-                  const SizedBox(height: 10),
-                  const Text(
-                    'You are One-Step Closer to creating your App',
-                    style: TextStyle(fontSize: 18, color: Colors.white),
+                ),
+                const SizedBox(height: 10), // Gap between card and container
+                Container(
+                  width: 344,
+                  height: 78,
+                  decoration: BoxDecoration(
+                    color: const Color.fromRGBO(85, 166, 196, 0.3),
+                    borderRadius: BorderRadius.circular(16),
                   ),
-             
-                  Padding(
-                    padding: const EdgeInsets.only(top: 20),
-                    child: ElevatedButton(
-                      onPressed: () {},
-                      child: const Text(
-                        'Complete Your App',
-                        style: TextStyle(fontSize: 18), 
-                      ),
-                      style: ButtonStyle(
-                        backgroundColor: MaterialStateProperty.all(
-                          Color.fromRGBO(184, 254, 34, 1),
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Column(
+                          children: const [
+                            Icon(Icons.person, color: Colors.white),
+                            SizedBox(height: 5),
+                            Text(
+                              'Item 1',
+                              style: TextStyle(color: Colors.white),
+                            ),
+                          ],
                         ),
-                        minimumSize: MaterialStateProperty.all(Size(screenWidth - 40, 50)),
-                        padding: MaterialStateProperty.all(EdgeInsets.symmetric(vertical: 16)),
-                        shape: MaterialStateProperty.all(
-                          RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(8), 
+                        Column(
+                          children: const [
+                            Icon(Icons.settings, color: Colors.white),
+                            SizedBox(height: 5),
+                            Text(
+                              'Item 2',
+                              style: TextStyle(color: Colors.white),
+                            ),
+                          ],
+                        ),
+                        Column(
+                          children: const [
+                            Icon(Icons.notifications, color: Colors.white),
+                            SizedBox(height: 5),
+                            Text(
+                              'Item 3',
+                              style: TextStyle(color: Colors.white),
+                            ),
+                          ],
+                        ),
+                        Column(
+                          children: const [
+                            Icon(Icons.info, color: Colors.white),
+                            SizedBox(height: 5),
+                            Text(
+                              'Item 4',
+                              style: TextStyle(color: Colors.white),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 10), // Gap between containers
+                Container(
+                  width: 344,
+                  height: 119,
+                  decoration: BoxDecoration(
+                    color: const Color.fromRGBO(85, 166, 196, 0.3),
+                    borderRadius: BorderRadius.circular(16),
+                  ),
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: Container(
+                          margin: const EdgeInsets.only(right: 8),
+                          decoration: BoxDecoration(
+                            color: const Color.fromRGBO(85, 166, 196, 0.3),
+                            borderRadius: BorderRadius.circular(16),
+                          ),
+                          child: Center(
+                            child:
+                                Icon(Icons.star, color: Colors.white, size: 30),
+                          ),
+                        ),
+                      ),
+                      Expanded(
+                        child: Container(
+                          margin: const EdgeInsets.only(left: 8),
+                          decoration: BoxDecoration(
+                            color: const Color.fromRGBO(85, 166, 196, 0.3),
+                            borderRadius: BorderRadius.circular(16),
+                          ),
+                          child: Center(
+                            child: Icon(Icons.favorite,
+                                color: Colors.white, size: 30),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 10), // Gap between containers
+                Container(
+                  width: 344,
+                  height: 72,
+                  decoration: BoxDecoration(
+                    color: const Color.fromRGBO(85, 166, 196, 0.3),
+                    borderRadius: BorderRadius.circular(8),
+                    border: Border.all(color: Colors.white, width: 2),
+                  ),
+                  padding: const EdgeInsets.all(10.0),
+                  child: Center(
+                    child: Icon(Icons.home, color: Colors.white, size: 30),
+                  ),
+                ),
+                const SizedBox(
+                    height:
+                        50), // Gap between content above and the bottom container
+                const SizedBox(
+                    height:
+                        50), // Gap between content above and the bottom container
+                Stack(
+                  clipBehavior:
+                      Clip.none, // Ensures elements outside are visible
+                  children: [
+                    Container(
+                      width: 280, // Fixed width
+                      height: 50, // Height
+                      decoration: BoxDecoration(
+                        color: Colors.blue, // Background color
+                        borderRadius:
+                            BorderRadius.circular(15), // Rounded corners
+                      ),
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 40), // Padding left and right
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Icon(Icons.home, color: Colors.white),
+                          SizedBox(
+                              width: 24), // Placeholder for the middle icon
+                          Icon(Icons.person, color: Colors.white),
+                        ],
+                      ),
+                    ),
+                    Positioned(
+                      top: -20, // Adjusted to align the circular boundary
+                      left: 120, // Centers the icon horizontally
+                      child: Container(
+                        width: 50, // Size of the circular boundary
+                        height: 50,
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          border: Border.all(
+                            color: Colors.blue, // Blue boundary color
+                            width: 3, // Thickness of the boundary
+                          ),
+                          color: Colors.blue, // Background color
+                        ),
+                        child: const Center(
+                          child: Icon(
+                            Icons.search,
+                            color: Colors.white,
+                            size: 30, // Icon size
                           ),
                         ),
                       ),
                     ),
-                  ),
-                ],
-              ),
+                  ],
+                ),
+              ],
             ),
           ),
         ),
